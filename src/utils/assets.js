f3a5f794f525a402f110aee29405ecfdafd9c28e
mohammed054 <mohammed.hassoun054@gmail.com>
2026-03-10 16:05:00 +0400

    clean main branch

--- src/utils/assets.js
+++
@@ -1,9 +0,0 @@
-const RAW_BASE_URL = import.meta.env.BASE_URL || '/'
-
-const BASE_URL = RAW_BASE_URL.endsWith('/') ? RAW_BASE_URL : `${RAW_BASE_URL}/`
-
-export function getAssetPath(path = '') {
-  const normalizedPath = String(path).replace(/^\/+/, '')
-  return `${BASE_URL}${normalizedPath}`
-}
-
